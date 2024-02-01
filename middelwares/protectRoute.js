8a04b705cd5c464492c464fbd9d123997e78a5c0
soubankunnummel <muhammedsoubankunnummel123@gmail.com>
2024-02-01 20:25:51 +0530

    fix:samesite

--- middelwares/protectRoute.js
+++ middelwares/protectRoute.js
@@ -4,8 +4,8 @@ import User from "../models/userModel.js";
 
     const protectRoute = async (req, res, next) => {
         try {
-            const token = req.cookies.jwt
-            // const token = req.headers["authorization"]
+            // const token = req.cookies.jwt
+            const token = req.headers["authorization"]
             
             if(!token)return res.status(401).json({message:"Unauthorized"})
 
--- utils/helpers/generateTokenAndSetCookie.js
+++ utils/helpers/generateTokenAndSetCookie.js
@@ -9,11 +9,8 @@ const generateTokenAndSetCookie = (userId, res) => {
     res.cookie("jwt", token, {
         httpOnly: true,
         maxAge: 15 * 24 * 60 * 1000,
-        sameSite: 'Lax', 
-    });
-    ;
-    
-    
+        sameSite:"none", 
+    })
     return token
 }
 export default generateTokenAndSetCookie
