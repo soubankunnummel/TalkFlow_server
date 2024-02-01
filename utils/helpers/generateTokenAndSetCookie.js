518c43cfcf130ddf60117a73027132f4323afcbf
soubankunnummel <muhammedsoubankunnummel123@gmail.com>
2024-02-01 12:49:37 +0530

    fix:jwt

--- utils/helpers/generateTokenAndSetCookie.js
+++ utils/helpers/generateTokenAndSetCookie.js
@@ -9,7 +9,8 @@ const generateTokenAndSetCookie = (userId, res) => {
     res.cookie("jwt", token, {
         httpOnly: true,
         maxAge: 15 * 24 * 60 * 1000,
-        sameSite:"strict", 
+        domain: ".onrender.com",
+        sameSite:"true", 
     })
     return token
 }
